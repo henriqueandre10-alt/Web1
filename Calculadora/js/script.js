25d1eaeafd75462963acc97eb5a13a73b281decb
henriqueandre10-alt <henrique.andre10@aluno.ifce.edu.br>
2025-11-20 20:54:17 -0300

    Add files via upload
    Confirmar alterações

---
+++ Calculadora/js/script.js
@@ -0,0 +1,13 @@
+const dpyElemento = document.getElementById('dpy');
+
+const um = document.getElementById('um');
+const dois = document.getElementById('dois');
+
+function exibirValor(){
+
+const numeroDoButton = this.dataset.numero;
+
+}
+
+Button1.addEventListtener('click', exibirNumero);
+Button2.addEventListtener('click', exibirNumero);
